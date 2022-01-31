8193126533fb4487f3397f0364cfa6fed7a0f887
unknown <gina.anifah.tif20@polban.ac.id>
2022-01-31 13:11:04 +0700

    latihan android

---
+++ Pertemuan1/latihan/app/src/main/java/com/example/firstlearning/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.firstlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this , MainActivity2.class); /*membuka ke layout yang baru (activity sekarang , activity tujuan)*/
+                startActivity(intent);
+            }
+        });
+    }
+}
